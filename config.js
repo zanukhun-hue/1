f084701691e1c5a80144c3da3703be42558931a2
zanukhun-hue <zanukhun@mail.ru>
2026-05-16 02:21:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-// Настройки сайта.
-// Для публичной статистики создайте код в GoatCounter и вставьте его ниже.
-// Пример: goatCounterCode: "my-ae-site" для https://my-ae-site.goatcounter.com
 window.AE_SITE_CONFIG = {
-  goatCounterCode: "",
+  goatCounterCode: "my-ae-plugins",
   telegramUrl: "https://t.me/ae_plugins_vault"
 };
