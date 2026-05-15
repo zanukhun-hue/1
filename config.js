9df6c20ff145ee5eb3acd081233f9c6f2a0aafeb
zanukhun-hue <zanukhun@mail.ru>
2026-05-16 02:30:41 +0300

    bulba2

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
+// Настройки сайта.
+// Для публичной статистики создайте код в GoatCounter и вставьте его ниже.
+// Пример: goatCounterCode: "my-ae-site" для https://my-ae-site.goatcounter.com
 window.AE_SITE_CONFIG = {
-  goatCounterCode: "my-ae-plugins",
+  goatCounterCode: "",
   telegramUrl: "https://t.me/ae_plugins_vault"
 };
